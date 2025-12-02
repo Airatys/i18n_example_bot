ab0369f1ccd9868030fbd2ecc66299ec6e65bef7
Airat Sharifullin <sharifullin.airat@yandex.ru>
2025-12-02 20:59:07 +0300

    add gettext as _

--- handlers/user.py
+++ handlers/user.py
@@ -7,6 +7,8 @@ from aiogram.types import (
     Message
 )
 
+from aiogram.utils.i18n import gettext as _
+
 # Инициализируем роутер уровня модуля
 user_router = Router()
 
@@ -17,14 +19,14 @@ async def process_start_command(message: Message):
     username = html.quote(message.from_user.full_name)
     # Создаем объект инлайн-кнопки
     button = InlineKeyboardButton(
-        text='Кнопка',
+        text=_('Кнопка'),
         callback_data='button_pressed'
     )
     # Создаем объект инлайн-клавиатуры
     markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
     # Отправляем сообщение пользователю
     await message.answer(
-        text='Привет, {username}. Нажмите на кнопку'.format(username=username),
+        text=_('Привет, {username}. Нажмите на кнопку').format(username=username),
         reply_markup=markup
     )
 
@@ -32,4 +34,4 @@ async def process_start_command(message: Message):
 # Этот хэндлер срабатывает на нажатие инлайн-кнопки
 @user_router.callback_query(F.data == 'button_pressed')
 async def process_button_click(callback: CallbackQuery):
-    await callback.answer(text='Вы нажали на кнопку')
+    await callback.answer(text=_('Вы нажали на кнопку'))
